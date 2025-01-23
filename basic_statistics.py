3cb9349c68e6f0895bc8da4106d5d63448f4946b
Veljko Petkovic <38298277+veljko2404@users.noreply.github.com>
2025-01-23 02:45:56 +0100

    basic statistics done

--- basic_statistics.py
+++ basic_statistics.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+from statistics import mode
 
 data = pd.read_csv("Bank_Churn.csv")
 
@@ -13,13 +13,21 @@ balance = np.asarray(data["Balance"])  # The customer's account balance
 has_credit_card = np.asarray(data["HasCrCard"])  # Whether the customer has a credit card (1 = yes, 0 = no)
 estimated_salary = np.asarray(data["EstimatedSalary"])  # The estimated salary of the customer
 
-plt.scatter(age, balance)
-plt.show()
-plt.scatter(credit_score, age)
-plt.show()
-plt.scatter(age, tenure)
-plt.show()
-plt.scatter(age, estimated_salary)
-plt.show()
-plt.scatter(age, gender)
-plt.show()
+all_data = {
+    "Credit Score": credit_score,
+    "Age": age,
+    "Tenure": tenure,
+    "Balance": balance,
+    "Estimated Salary": estimated_salary,
+}
+
+for key, value in all_data.items():
+    print(f"{key}")
+    print(f"Mean: {value.mean():.2f}")
+    print(f"Median: {np.median(value):.2f}")
+    print(f"Mode: {mode(value):.2f}")
+    print(f"Min: {value.min():.2f}")
+    print(f"Max: {value.max():.2f}")
+    print(f"25th percentile: {np.quantile(value, 0.25):.2f}")
+    print(f"75th percentile: {np.quantile(value, 0.75):.2f}")
+    print(f"Standard Deviation: {value.std():.2f}\n")
