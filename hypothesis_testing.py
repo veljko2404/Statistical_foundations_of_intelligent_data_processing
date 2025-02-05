2f5e6bb643f9205e729ab9b0da48f758c6a80075
Veljko Petkovic <38298277+veljko2404@users.noreply.github.com>
2025-02-05 01:39:23 +0100

    hypothesis testing done

--- hypothesis_testing.py
+++ hypothesis_testing.py
@@ -10,7 +10,8 @@ gender = np.asarray(data["Gender"])
 balance = np.asarray(data["Balance"])
 estimated_salary = np.asarray(data["EstimatedSalary"])
 
-# First we will check if estimated salary is significantly different in each
+# One-Sample T-Test
+# We will check if estimated salary is significantly different in each
 # of the 3 countries from data (France, Germany and Spain)
 
 estimated_salary_france = estimated_salary[geography == "France"]
@@ -23,7 +24,7 @@ salaries = {
     "Germany": estimated_salary_germany
 }
 
-print(f"Overall mean estimated salary: {estimated_salary.mean():.2f}")
+print(f"Overall mean estimated salary: {estimated_salary.mean():.2f}\n")
 
 for country, salary in salaries.items():
     print(f"Mean estimated salary in {country}: {salary.mean():.2f}")
@@ -38,3 +39,10 @@ for country, salary in salaries.items():
     sigma = salary.std(ddof=1) / math.sqrt(salary.size)
     conf_int = stats.t.interval(0.95, df=salary.size - 1, loc=salary.mean(), scale=sigma)
     print(f"95% Confidence Interval: [{conf_int[0]:.2f}, {conf_int[1]:.2f}]\n")
+
+
+# Two-Sample T-Test
+# Here we will check whether means of France and Spain differ
+
+print(stats.ttest_ind(a=estimated_salary_france, b=estimated_salary_spain, equal_var=False))
+
